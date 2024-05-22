a2a99a517c6329eed511510d1135c235351e3d52
elzenaro <nam2626@naver.com>
2024-05-22 14:54:05 +0900

    servlet

---
+++ Step03/src/main/java/servlet/FirstServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FirstServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String str = request.getParameter("txt");
+		request.setAttribute("txt", str + "서버에서 데이터 추가한 부분");
+		//페이지 이동 처리
+		request.getRequestDispatcher("first_servlet_result.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Step03/src/main/webapp/01_first_sevlet.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>서블릿 호출</title>
+</head>
+<body>
+	<form action="FirstServlet">
+		<input type="text" name="txt">
+		<button>전송</button>
+	</form>
+</body>
+</html>
