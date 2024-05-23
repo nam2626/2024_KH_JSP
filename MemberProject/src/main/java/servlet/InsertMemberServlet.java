abeac29f91afa4c9228c0f0abc3923bcdd648427
elzenaro <nam2626@naver.com>
2024-05-23 15:55:29 +0900

    회원 정보 등록

--- MemberProject/src/main/java/servlet/InsertMemberServlet.java
+++ MemberProject/src/main/java/servlet/InsertMemberServlet.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import dao.BoardMemberDAO;
+import dto.BoardMemberDTO;
+
 /**
  * Servlet implementation class InsertMemberServlet
  */
@@ -27,19 +30,34 @@ public class InsertMemberServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//1. 사용자 보낸 데이터를 받음 -> request.getParameter("input 태그의 네임속성값"); 
+		String boardMemberId = request.getParameter("member_id");
+		String boardMemberName = request.getParameter("member_name");
+		String boardMemberPasswd = request.getParameter("member_passwd");
+		String boardMemberNick= request.getParameter("member_nick");
 		
 		//2. BoardMemberDTO를 생성
+		//	 콘솔에 생성한 DTO 출력
+		BoardMemberDTO dto = new BoardMemberDTO(boardMemberId, boardMemberName, 
+												boardMemberPasswd, boardMemberNick);
+		System.out.println(dto.toString());
 		
 		//3. BoardMemberDAO에 회원정보 삽입하는 메서드 호출
-		
+		int row = BoardMemberDAO.getInstance().insertBoardMember(dto);
+		if(row == 0) {
+			System.out.println("데이터 등록 실패");
+		}else {
+			System.out.println("데이터 등록 성공");
+		}
 		//4. 전체회원정보 페이지로 이동
+		response.sendRedirect("./allMember");//서블릿에서 다른 서블릿을 호출 가능
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//post 보내면 구버전 톰캣의 경우 한글처리가 안되서 깨짐, 다시 인코딩 해줘야함
+//		request.setCharacterEncoding("utf-8");
 		doGet(request, response);
 	}
 
