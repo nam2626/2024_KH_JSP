4fb1a41bee395bb781b0bf8b375b3abd3149f99e
elzenaro <nam2626@naver.com>
2024-05-23 15:22:06 +0900

    전체 회원 조회

---
+++ MemberProject/src/main/java/servlet/InsertMemberServlet.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class InsertMemberServlet
+ */
+@WebServlet("/insertMember")
+public class InsertMemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertMemberServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//1. 사용자 보낸 데이터를 받음 -> request.getParameter("input 태그의 네임속성값"); 
+		
+		//2. BoardMemberDTO를 생성
+		
+		//3. BoardMemberDAO에 회원정보 삽입하는 메서드 호출
+		
+		//4. 전체회원정보 페이지로 이동
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
