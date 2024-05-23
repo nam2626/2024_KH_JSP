7281543158991564959683d1165620504d93c36d
elzenaro <nam2626@naver.com>
2024-05-23 12:04:55 +0900

    JDBC Test

--- MemberProject/src/main/java/dao/MemberDAO.java
+++ MemberProject/src/main/java/dao/MemberDAO.java
@@ -1,5 +1,8 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import oracle.jdbc.pool.OracleDataSource;
@@ -26,7 +29,34 @@ public class MemberDAO {
 		return instance;
 	}
 
+	//전체 회원정보 조회
+	public void selectAllMember() {
+		String sql = "select * from board_member";
+		
+		//접속 정보 획득 후 SQL 실행할 준비
+		try(Connection conn = ods.getConnection();
+			PreparedStatement pstmt = conn.prepareStatement(sql);){
+			
+			//SQL문 실행
+			try(ResultSet rs = pstmt.executeQuery()){
+				while(rs.next()) {
+					System.out.println(rs.getString(1) + " " + rs.getString(2));
+				}
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 	
 	
 }
+
+
+
+
+
+
+
