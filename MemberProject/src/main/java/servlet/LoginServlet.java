c976ae7dc92eda2f9eced34710878b045076bf5c
elzenaro <nam2626@naver.com>
2024-05-28 11:43:29 +0900

    로그인 처리

--- MemberProject/src/main/java/servlet/LoginServlet.java
+++ MemberProject/src/main/java/servlet/LoginServlet.java
@@ -36,7 +36,22 @@ public class LoginServlet extends HttpServlet {
 		
 		//dao에 아이디 비밀번호 넘겨서 검색해서 검색 결과를 BoardMemberDTO로 받음
 		BoardMemberDTO dto = BoardMemberDAO.getInstance().login(id, passwd);
-		System.out.println(dto.toString());
+		System.out.println(dto);
+		
+		if(dto != null) {
+			//페이지 이동처리
+			//   로그인 성공시 ./allMember
+			//request.getRequestDispatcher("./allMember").forward(request, response);
+			response.sendRedirect("./allMember");
+		}else {
+			// 로그인 실패시
+			//	 아이디 비밀번호를 확인하세요, 이전페이지로 이동
+			response.setContentType("text/html;charset=utf-8");
+			response.getWriter().println("<script>"
+					+ "alert('아이디와 비밀번호 확인하세요');"
+					+ "history.back();"
+					+ "</script>");
+		}
 	}
 
 	/**
