af66a68649de609d7258131d3f0caf79f5c51593
elzenaro <nam2626@naver.com>
2024-05-29 14:01:17 +0900

    MVC

---
+++ MemberMVCProject/src/main/java/controller/Controller.java
@@ -0,0 +1,9 @@
+package controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import view.ModelAndView;
+
+public interface Controller {
+	public ModelAndView execute(HttpServletRequest request, HttpServletResponse response);
+}
