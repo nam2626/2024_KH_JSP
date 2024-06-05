90fe74cc8d6713cf4c44304fe535cbf946c97843
elzenaro <nam2626@naver.com>
2024-06-05 11:13:38 +0900

    좋아요/싫어요

--- BoardProject/src/main/java/controller/BoardLikeController.java
+++ BoardProject/src/main/java/controller/BoardLikeController.java
@@ -24,23 +24,23 @@ public class BoardLikeController implements Controller {
 		if (bno == 0) {
 			json.put("code", 1);
 			json.put("msg", "게시글 번호가 없습니다.");
-		}
-		if (dto == null) {
+		}else if (dto == null) {
 			json.put("code", 2);
 			json.put("msg", "로그인 하셔야 이용할 수 있습니다.");
-		}
-		//board_like 테이블에 사용자ID/글번호를 insert
-		try {
-			//좋아요가 정상 처리 되었을때
-			BoardService.getInstance().insertBoardLike(bno, dto.getBoardMemberId());
-			json.put("code", 0);
-			json.put("msg", "해당 게시글에 좋아요를 하셨습니다.");
-		}catch (Exception e) {
-			//이미 좋아요를 했을 떄 처리
-			//board_like 테이블에 사용자ID/글번호를 delete
-			BoardService.getInstance().deleteBoardLike(bno, dto.getBoardMemberId());
-			json.put("code", 0);
-			json.put("msg", "해당 게시글에 좋아요를 취소 하셨습니다.");
+		}else {
+			//board_like 테이블에 사용자ID/글번호를 insert
+			try {
+				//좋아요가 정상 처리 되었을때
+				BoardService.getInstance().insertBoardLike(bno, dto.getBoardMemberId());
+				json.put("code", 0);
+				json.put("msg", "해당 게시글에 좋아요를 하셨습니다.");
+			}catch (Exception e) {
+				//이미 좋아요를 했을 떄 처리
+				//board_like 테이블에 사용자ID/글번호를 delete
+				BoardService.getInstance().deleteBoardLike(bno, dto.getBoardMemberId());
+				json.put("code", 0);
+				json.put("msg", "해당 게시글에 좋아요를 취소 하셨습니다.");
+			}
 		}
 		json.put("count", BoardService.getInstance().selectBoardLikeCount(bno));
 		
