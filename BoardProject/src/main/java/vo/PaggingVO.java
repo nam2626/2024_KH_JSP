53bc1577cd67cd8883db683410f94d49a0f7ef48
elzenaro <nam2626@naver.com>
2024-06-05 14:00:24 +0900

    페이징

--- BoardProject/src/main/java/vo/PaggingVO.java
+++ BoardProject/src/main/java/vo/PaggingVO.java
@@ -20,11 +20,23 @@ public class PaggingVO {
 		return currentPage;
 	}
 
-	//전체 페이지 개수
+	//전체 페이지 개수 : 전체 게시글 개수 / 한페이지당 출력할 게시글 개수 + (나머지가 0 아니면 1)
 	public int getTotalPage() {
-		
+		return count / pageOfContentCount + (count % pageOfContentCount == 0 ? 0 : 1);
+	}
+	
+	//전체 페이지 그룹 개수 : 전체 페이지 개수 / 게시판 하단에 나타낼 페이지 번호 개수 + (나머지가 0 아니면 1)
+	public int getTotalPageGroup() {
+		return getTotalPage() / PAGE_GROUP_OF_COUNT + 
+				(getTotalPage() % PAGE_GROUP_OF_COUNT == 0 ? 0 : 1);
 	}
 	
 	
-	
 }
+
+
+
+
+
+
+
