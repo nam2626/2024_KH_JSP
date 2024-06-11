4c6de501e2b5c67fa388bcd21c385922b6c38d5d
elzenaro <nam2626@naver.com>
2024-06-11 11:06:50 +0900

    댓글 좋아요/싫어요

--- BoardProject/src/main/java/controller/BoardCommentLikeController.java
+++ BoardProject/src/main/java/controller/BoardCommentLikeController.java
@@ -2,6 +2,8 @@ package controller;
 
 import java.io.IOException;
 
+import org.json.JSONObject;
+
 import dto.BoardMemberDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -15,12 +17,17 @@ public class BoardCommentLikeController implements Controller {
 		BoardMemberDTO dto = (BoardMemberDTO) request.getSession().getAttribute("user");
 		int cno = Integer.parseInt(request.getParameter("cno"));
 		response.setContentType("text/html;charset=utf-8");
+		JSONObject json = new JSONObject();
 		try {
 			BoardService.getInstance().insertBoardCommentLike(dto.getBoardMemberId(), cno);
-			response.getWriter().println("{result : 'insert'}");
+			json.put("result", "insert");
+			System.out.println(json.toString());
+			response.getWriter().println(json.toString());
 		}catch (Exception e) {
 			BoardService.getInstance().deleteBoardCommentLike(dto.getBoardMemberId(), cno);
-			response.getWriter().println("{result : 'delete'}");
+			json.put("result", "delete");
+			System.out.println(json.toString());
+			response.getWriter().println(json.toString());
 		}
 		
 		
