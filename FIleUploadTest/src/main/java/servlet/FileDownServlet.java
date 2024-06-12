c105d1feca551cbe9dcdf251a4530a6f579f2e1c
elzenaro <nam2626@naver.com>
2024-06-12 09:20:30 +0900

    파일 다운로드

---
+++ FIleUploadTest/src/main/java/servlet/FileDownServlet.java
@@ -0,0 +1,63 @@
+package servlet;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FileDownServlet
+ */
+@WebServlet("/fileDown.do")
+public class FileDownServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FileDownServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int bno = Integer.parseInt(request.getParameter("bno"));
+		int fno = Integer.parseInt(request.getParameter("fno"));
+		
+		File file = new File("c:\\fileupload\\3180046.jpg");
+		response.setHeader("Content-Disposition", "attachement;fileName="+file.getName());
+		response.setHeader("Content-Transfer-Encoding", "binary");
+		response.setContentLength((int)file.length());
+		
+		FileInputStream fis = new FileInputStream(file);
+		BufferedOutputStream bos = new BufferedOutputStream(response.getOutputStream());
+		byte[] buffer = new byte[1024*1024];
+		while(true) {
+			int size = fis.read(buffer);
+			if(size == -1) break;
+			bos.write(buffer,0,size);
+			bos.flush();
+		}
+		fis.close();
+		bos.close();
+	
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
